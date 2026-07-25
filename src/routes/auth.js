206fc2655f9cce327b8708e4f22c9d50ac9d6600
Jern <cerenbulbul27@gmail.com>
2026-07-25 16:20:49 +0200

    cookie

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -97,15 +97,13 @@ router.post("/login", async (req, res) => {
       }
     );
 
-    const isProduction = process.env.NODE_ENV === "production";
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: isProduction,
-      sameSite: isProduction ? "none" : "lax",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
-
     res.json({
       message: "Login successful.",
       user: {
