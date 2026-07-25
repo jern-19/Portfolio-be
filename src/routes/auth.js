04a44852918a2ffc047ae7cf7387c9807947a6b6
Jern <cerenbulbul27@gmail.com>
2026-07-26 01:02:02 +0200

    update me

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -118,11 +118,33 @@ router.post("/login", async (req, res) => {
   }
 });
 
-router.get("/me", auth, (req, res) => {
+router.get("/me", auth, async (req, res) => {
 
-  res.json({
-    user: req.user
-  });
+  try {
+
+    const user = await User.findById(req.user.id)
+      .select("-password");
+
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found"
+      });
+    }
+
+
+    res.json({
+      user
+    });
+
+
+  } catch(error){
+
+    res.status(500).json({
+      message:"Server error"
+    });
+
+  }
 
 });
 
